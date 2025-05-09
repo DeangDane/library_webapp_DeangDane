92732f16180d82e882e01f45dd1db3f81261d557
DeangDane <deangdane9@gmail.com>
2025-05-09 11:10:17 +0700

    first commit

---
+++ src/main/java/gic/i4/library_webapp_DeangDane/LibraryWebappDeangDaneApplication.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_DeangDane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebappDeangDaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebappDeangDaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gic/i4/library_webapp_DeangDane/LibraryWebappDeangDaneApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_DeangDane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebappDeangDaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
